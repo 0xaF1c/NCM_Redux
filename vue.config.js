fd12fe4d8d8d1aa65d505dbdb986d42477a33f91
0xaF1c <3512803782@qq.com>
2023-08-22 22:16:05 +0800

    change api source

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://netease-cloud-music-redux-api.vercel.app/',
         changeOrigin: true, 
         secure: false,
         ws: true,
